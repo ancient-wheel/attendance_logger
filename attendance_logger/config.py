0efbc39665aa0883d35b0c2382096f80a177fec8
Andrey Antiporovich <andrey.antiporovich@protonmail.com>
2026-01-30 10:24:52 +0300

    refactoring config file; add SERVER_NAME configuration

--- attendance_logger/config.py
+++ attendance_logger/config.py
@@ -13,22 +13,21 @@ else:
 # General Config
 class Config:
 
-    # Security configuration
-    SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
+    # Flask configuration
+    SERVER_NAME = os.environ["SERVER_NAME"]
+    SECRET_KEY = os.environ["SECRET_KEY"]
 
-    FAKER_LOCALE = "ru_RU"
-    
     # Email configuration
     EMAIL_SENDER = os.environ["EMAIL_SENDER"]
     EMAIL_SMTP_SERVER = os.environ["EMAIL_SMTP_SERVER"]
-    EMAIL_SMTP_PORT = int(os.environ.get("EMAIL_SMTP_PORT", 465))
+    EMAIL_SMTP_PORT = int(os.environ.get("EMAIL_SMTP_PORT"))
     EMAIL_PASSWORD = os.environ["EMAIL_PASSWORD"]
 
 
 class DevConfig(Config):
     """Set Flask config variables."""
 
-    # General
+    # Flask configuration
     APP_DEBUG = True
 
     # Flask-SQLALCHEMY
@@ -37,6 +36,9 @@ class DevConfig(Config):
 
     APP_ADMIN_EMAIL = os.environ["ADMIN_EMAIL"]
     APP_ADMIN_DEFAULT_PASSWORD = os.environ["ADMIN_PASSWORD"]
+
+    # Faker configuration
+    FAKER_LOCALE = "ru_RU"
 
 
 class ProdConfig(Config):
